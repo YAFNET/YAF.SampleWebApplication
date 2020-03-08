6db30a6b19f322729067513b69e24325a02bc209
w8tcha <thewatcher@watchersnet.de>
2020-03-08 01:46:45 +0100

    upgraded to 2.31.02

--- YAF.SampleWebApplication/Properties/AssemblyInfo.cs
+++ YAF.SampleWebApplication/Properties/AssemblyInfo.cs
@@ -54,5 +54,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.3.1.01")]
-[assembly: AssemblyFileVersion("2.3.1.01")]
+[assembly: AssemblyVersion("2.3.1.02")]
+[assembly: AssemblyFileVersion("2.3.1.02")]
