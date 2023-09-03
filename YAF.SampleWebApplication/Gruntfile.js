322c10f4ba7da265d0a77908341d7c4148d522d5
Ingo <thewatcher@watchersnet.de>
2023-09-03 08:36:33 +0200

    added yaf upgrade script

--- YAF.SampleWebApplication/Gruntfile.js
+++ YAF.SampleWebApplication/Gruntfile.js
@@ -12,7 +12,107 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
 
+        downloadfile: {
+            options: {
+                dest: "./",
+                overwriteEverytime: true
+            },
+            files: {
+                'YAF.SqlServer-Upgrade.zip': "https://github.com/YAFNET/YAFNET/releases/download/v<%= pkg.version %>/YAF.SqlServer-v<%= pkg.version %>-Upgrade.zip"
+            }
+        },
+
+        unzip: {
+            "upgrade/": "YAF.SqlServer-Upgrade.zip"
+        },
+
+        shell: {
+            delete: {
+                command: [
+                    "@echo off",
+                    "rmdir upgrade /s /q ",
+                    'del "YAF.SqlServer-Upgrade.zip"'
+                ].join("&&")
+            }
+        },
+
         copy: {
+            upgrade: {
+                files: [
+                    {
+                        expand: true,
+                        src: "**/*.dll",
+                        cwd: "upgrade/bin",
+                        dest: "bin/"
+                    },
+                    {
+                        expand: true,
+                        src: "**/*.*",
+                        cwd: "upgrade/Content/",
+                        dest: "forum/Content/"
+                    },
+                    {
+                        expand: true,
+                        src: "**/*.*",
+                        cwd: "upgrade/Controls/",
+                        dest: "forum/Controls/"
+                    },
+                    {
+                        expand: true,
+                        src: "**/*.*",
+                        cwd: "upgrade/Dialogs/",
+                        dest: "forum/Dialogs/"
+                    },
+                    {
+                        expand: true,
+                        src: "**/*.*",
+                        cwd: "upgrade/Images/",
+                        dest: "forum/Images/"
+                    },
+                    {
+                        expand: true,
+                        src: "**/*.*",
+                        cwd: "upgrade/Install/",
+                        dest: "forum/Install/"
+                    },
+                    {
+                        expand: true,
+                        src: "**/*.*",
+                        cwd: "upgrade/Content/",
+                        dest: "forum/Content/"
+                    },
+                    {
+                        expand: true,
+                        src: "**/*.*",
+                        cwd: "upgrade/languages/",
+                        dest: "forum/languages/"
+                    },
+                    {
+                        expand: true,
+                        src: "**/*.*",
+                        cwd: "upgrade/Pages/",
+                        dest: "forum/Pages/"
+                    },
+                    {
+                        expand: true,
+                        src: "**/*.*",
+                        cwd: "upgrade/Resources/",
+                        dest: "forum/Resources/"
+                    },
+                    {
+                        expand: true,
+                        src: "**/*.*",
+                        cwd: "upgrade/Scripts/",
+                        dest: "forum/Scripts/"
+                    },
+                    {
+                        expand: true,
+                        src: "**/digest.aspx",
+                        cwd: "upgrade/",
+                        dest: "forum/"
+                    }
+                ]
+            },
             emojionearea: {
                 files: [
                     {
@@ -69,9 +169,12 @@ module.exports = function(grunt) {
     // PLUGINS
     grunt.loadNpmTasks("grunt-contrib-copy");
     grunt.loadNpmTasks("grunt-dev-update");
+    grunt.loadNpmTasks("grunt-zip");
+    grunt.loadNpmTasks("grunt-downloadfile");
+    grunt.loadNpmTasks("grunt-shell");
 
     grunt.registerTask("default",
         [
-            "devUpdate", "copy"
+            "devUpdate", "downloadfile", "unzip", "copy", "shell"
         ]);
 };
